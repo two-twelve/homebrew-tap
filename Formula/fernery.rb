4e557ce2d920ff8a276cecef8355dfb4d5886395
Freddy Heppell <freddy@hey.com>
2023-04-07 13:21:14 +0000

    fernery: add 0.1.0 bottle.

--- Formula/fernery.rb
+++ Formula/fernery.rb
@@ -5,6 +5,12 @@ class Fernery < Formula
   sha256 "f8beffda2888eaa2645efbea2753cb388310ac2f72885c6db00c7dd33fc72a08"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/two-twelve/homebrew-tap/releases/download/fernery-0.1.0"
+    sha256 cellar: :any_skip_relocation, monterey:     "16dbdec02689b3b4cbefacfc04ea3b805af1a283a8bbffaaf0b08a617024bdda"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "ef4b5d1f3535bce5c9891f7dfaf52b63bf0ae38e0569b9b18bb243a63aba3f81"
+  end
+
   # GHC shouldn't be needed on test (https://github.com/orgs/Homebrew/discussions/4284)
   depends_on "ghc@9.2" => [:build, :test]
   depends_on "haskell-stack" => [:build, :test]
