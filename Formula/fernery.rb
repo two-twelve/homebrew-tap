3d13fc4420d3fdede3efa7d918b09c57a94e402c
Freddy Heppell <freddy@freddyheppell.com>
2023-04-07 11:13:08 +0100

    Fix style issues in formula

--- Formula/fernery.rb
+++ Formula/fernery.rb
@@ -1,12 +1,12 @@
 class Fernery < Formula
-  desc "A CLI tool for generating images of ferns 🌿 and other Iterated Function Systems"
+  desc "CLI tool for generating images of ferns 🌿 and other Iterated Function Systems"
   homepage "https://github.com/two-twelve/fernery"
   url "https://github.com/two-twelve/fernery/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "f8beffda2888eaa2645efbea2753cb388310ac2f72885c6db00c7dd33fc72a08"
   license "MIT"
 
-  depends_on "haskell-stack" => :build
   depends_on "ghc@9.2" => :build
+  depends_on "haskell-stack" => :build
 
   def install
     stack_args = [
@@ -16,11 +16,11 @@ class Fernery < Formula
     ]
 
     system "stack", "build", *stack_args
-    system "mv", "#{bin}/fernery-exe", "#{bin}/fernery"
+    mv "#{bin}/fernery-exe", "#{bin}/fernery"
   end
 
   test do
-    system "fernery", "-f", "barnsley"
+    system "#{bin}/fernery", "-f", "barnsley"
     assert_predicate testpath/"fern.png", :exist?
   end
 end
